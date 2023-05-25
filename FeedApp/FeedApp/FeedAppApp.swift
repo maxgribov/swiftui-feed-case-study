2a35623a2ea11235c9804420da2ac016f8acd2b6
Max Gribov <gribov.max@gmail.com>
2023-05-25 22:39:34 +0300

    FeedApp added

---
+++ FeedApp/FeedApp/FeedAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FeedAppApp.swift
+//  FeedApp
+//
+//  Created by Max Gribov on 25.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct FeedAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
