5d343f39c23b1badf455e6bf5270b2ae454ac628
Max Gribov <gribov.max@gmail.com>
2023-05-25 23:00:01 +0300

    FeedItem added

--- FeedAppPackage/Package.swift
+++ FeedAppPackage/Package.swift
@@ -7,9 +7,7 @@ let package = Package(
     name: "FeedAppPackage",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "FeedAppPackage",
-            targets: ["FeedAppPackage"]),
+        .library(name: "Feed", targets: ["Feed"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,10 +17,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "FeedAppPackage",
-            dependencies: []),
-        .testTarget(
-            name: "FeedAppPackageTests",
-            dependencies: ["FeedAppPackage"]),
+            name: "Feed", dependencies: []),
+        .testTarget(name: "FeedTests", dependencies: ["Feed"]),
     ]
 )
---
+++ FeedAppPackage/Sources/Feed/Feature/FeedItem.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  
+//
+//  Created by Max Gribov on 25.05.2023.
+//
+
+import Foundation
+
+struct FeedItem {
+    
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
