55a62ded074c5c93b942f313ea9aa3a92c1ec5b8
Max Gribov <gribov.max@gmail.com>
2023-05-27 23:08:37 +0300

    Simplify spy

--- FeedAppPackage/Tests/FeedTests/RemoteFeedLoaderTests.swift
+++ FeedAppPackage/Tests/FeedTests/RemoteFeedLoaderTests.swift
@@ -15,7 +15,7 @@ final class RemoteFeedLoaderTests: XCTestCase {
         let url = URL(string: "https://a-given-url.com")!
         let (_, client) = makeSUT(url: url)
 
-        XCTAssertNil(client.requestedURL)
+        XCTAssertTrue(client.requestedURLs.isEmpty)
     }
     
     func test_load_requestsDataFromURL() {
@@ -25,7 +25,7 @@ final class RemoteFeedLoaderTests: XCTestCase {
         
         sut.load()
         
-        XCTAssertEqual(client.requestedURL, url)
+        XCTAssertEqual(client.requestedURLs, [url])
     }
     
     func test_loadTwice_requestsDataFromURLTwice() {
@@ -51,12 +51,10 @@ final class RemoteFeedLoaderTests: XCTestCase {
     
     private class HTTPClientSpy: HTTPClient {
         
-        var requestedURL: URL?
         var requestedURLs = [URL]()
         
         func get(from url: URL) {
             
-            requestedURL = url
             requestedURLs.append(url)
         }
     }
