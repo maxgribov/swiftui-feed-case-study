2a788dc9e7cf4dfb8bac94e82ad1be6d474adf2f
Max Gribov <gribov.max@gmail.com>
2023-05-27 23:06:45 +0300

    Guarantee that we can load more than once

--- FeedAppPackage/Tests/FeedTests/RemoteFeedLoaderTests.swift
+++ FeedAppPackage/Tests/FeedTests/RemoteFeedLoaderTests.swift
@@ -18,7 +18,7 @@ final class RemoteFeedLoaderTests: XCTestCase {
         XCTAssertNil(client.requestedURL)
     }
     
-    func test_load_requestDataFromURL() {
+    func test_load_requestsDataFromURL() {
         
         let url = URL(string: "https://a-given-url.com")!
         let (sut, client) = makeSUT(url: url)
@@ -26,6 +26,17 @@ final class RemoteFeedLoaderTests: XCTestCase {
         sut.load()
         
         XCTAssertEqual(client.requestedURL, url)
+    }
+    
+    func test_loadTwice_requestsDataFromURLTwice() {
+        
+        let url = URL(string: "https://a-given-url.com")!
+        let (sut, client) = makeSUT(url: url)
+        
+        sut.load()
+        sut.load()
+        
+        XCTAssertEqual(client.requestedURLs, [url, url])
     }
     
     //MARK: - Helpers
@@ -41,10 +52,12 @@ final class RemoteFeedLoaderTests: XCTestCase {
     private class HTTPClientSpy: HTTPClient {
         
         var requestedURL: URL?
+        var requestedURLs = [URL]()
         
         func get(from url: URL) {
             
             requestedURL = url
+            requestedURLs.append(url)
         }
     }
 }
