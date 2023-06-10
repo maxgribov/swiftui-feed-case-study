63f95ec8b70ec1c6a65d4023e9c4df34e45cc57d
Max Gribov <gribov.max@gmail.com>
2023-06-10 12:51:12 +0300

    Improve readability with helper method

--- FeedAppPackage/Tests/FeedTests/RemoteFeedLoaderTests.swift
+++ FeedAppPackage/Tests/FeedTests/RemoteFeedLoaderTests.swift
@@ -47,7 +47,7 @@ final class RemoteFeedLoaderTests: XCTestCase {
         sut.load { capturedErrors.append($0) }
         
         let clientError = NSError(domain: "Test", code: 0)
-        client.completions[0](clientError)
+        client.complete(with: clientError)
         
         XCTAssertEqual(capturedErrors, [.connectivity])
     }
@@ -71,6 +71,11 @@ final class RemoteFeedLoaderTests: XCTestCase {
             
             completions.append(completion)
             requestedURLs.append(url)
+        }
+        
+        func complete(with error: Error, at index: Int = 0) {
+            
+            completions[index](error)
         }
     }
 }
