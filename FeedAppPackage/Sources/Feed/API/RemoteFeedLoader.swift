130e61d1107b2e8044731158db02600ceaeef3e9
Max Gribov <gribov.max@gmail.com>
2023-06-10 12:37:10 +0300

    Delivers connectivity error on client's error

--- FeedAppPackage/Sources/Feed/API/RemoteFeedLoader.swift
+++ FeedAppPackage/Sources/Feed/API/RemoteFeedLoader.swift
@@ -9,7 +9,7 @@ import Foundation
 
 public protocol HTTPClient {
     
-    func get(from url: URL)
+    func get(from url: URL, completion: @escaping (Error) -> Void)
 }
 
 public final class RemoteFeedLoader {
@@ -17,14 +17,21 @@ public final class RemoteFeedLoader {
     private let url: URL
     private let client: HTTPClient
     
+    public enum Error: Swift.Error {
+        case connectivity
+    }
+    
     public init(url: URL, client: HTTPClient) {
         
         self.url = url
         self.client = client
     }
     
-    public func load() {
+    public func load(completion: @escaping (Error) -> Void = { _ in }) {
         
-        client.get(from: url)
+        client.get(from: url) { error in
+            
+            completion(.connectivity)
+        }
     }
 }
