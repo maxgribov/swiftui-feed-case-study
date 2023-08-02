bf8617dd8f0630a82f5e5fdde3373fa51fb9bacd
Max Gribov <gribov.max@gmail.com>
2023-08-02 19:13:18 +0300

    save command requests cache deletion

--- FeedAppPackage/Tests/FeedTests/Cache/CacheFeedUseCaseTests.swift
+++ FeedAppPackage/Tests/FeedTests/Cache/CacheFeedUseCaseTests.swift
@@ -6,17 +6,31 @@
 //
 
 import XCTest
+import Feed
 
 class LocalFeedLoader {
     
+    private let store: FeedStore
+    
     init(store: FeedStore) {
         
+        self.store = store
+    }
+    
+    func save(_ items: [FeedItem]) {
+        
+        store.deleteCachedFeed()
     }
 }
 
 class FeedStore {
     
     var deleteCachedFeedCallCount = 0
+    
+    func deleteCachedFeed() {
+        
+        deleteCachedFeedCallCount += 1
+    }
 }
 
 final class CacheFeedUseCaseTests: XCTestCase {
@@ -27,5 +41,27 @@ final class CacheFeedUseCaseTests: XCTestCase {
         let _ = LocalFeedLoader(store: store)
         
         XCTAssertEqual(store.deleteCachedFeedCallCount, 0)
+    }
+    
+    func test_save_requestsCacheDeletion() {
+        
+        let store = FeedStore()
+        let sut = LocalFeedLoader(store: store)
+        
+        let items = [uniqueFeedItem(), uniqueFeedItem()]
+        sut.save(items)
+        
+        XCTAssertEqual(store.deleteCachedFeedCallCount, 1)
+    }
+    
+    //MARK: - Helpers
+    
+    func uniqueFeedItem() -> FeedItem {
+        
+        FeedItem(id: UUID(), imageURL: anyURL())
+    }
+    private func anyURL() -> URL {
+        
+        URL(string: "http://some-url.com")!
     }
 }
