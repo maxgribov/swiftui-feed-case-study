bd5dcda1e7acc36718bd3e0cc7966e14c329d74a
Max Gribov <gribov.max@gmail.com>
2023-08-03 13:31:36 +0300

    Load command deletes more than seven days cache upon retrieval

--- FeedAppPackage/Tests/FeedTests/Cache/LoadFeedFromCacheUseCaseTests.swift
+++ FeedAppPackage/Tests/FeedTests/Cache/LoadFeedFromCacheUseCaseTests.swift
@@ -134,6 +134,20 @@ final class LoadFeedFromCacheUseCaseTests: XCTestCase {
         XCTAssertEqual(store.receivedMessages, [.retrieve, .deleteCachedFeed])
     }
     
+    func test_load_deletesCacheOnMoreThanSevenDaysOldCache() {
+        
+        let fixedCurrentDate = Date()
+        let (sut, store) = makeSUT(currentDate: { fixedCurrentDate })
+        let feed = uniqueFeedItems()
+        
+        let moreThanSevenDaysOldTimestamp = fixedCurrentDate.adding(days: -7).adding(seconds: -1)
+        
+        sut.load { _ in }
+        store.completeRetrieval(with: feed.local, timestamp: moreThanSevenDaysOldTimestamp)
+        
+        XCTAssertEqual(store.receivedMessages, [.retrieve, .deleteCachedFeed])
+    }
+    
     //MARK: - Helpers
     
     private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #filePath, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStoreSpy) {
