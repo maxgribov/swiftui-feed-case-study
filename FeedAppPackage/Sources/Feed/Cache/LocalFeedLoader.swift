33b9e782d2485eb4cc0fb3062447a33c072ee5a5
Max Gribov <gribov.max@gmail.com>
2023-08-03 13:22:36 +0300

    Deletes cache on retrieval error

--- FeedAppPackage/Sources/Feed/Cache/LocalFeedLoader.swift
+++ FeedAppPackage/Sources/Feed/Cache/LocalFeedLoader.swift
@@ -45,6 +45,7 @@ public final class LocalFeedLoader {
             
             switch result {
             case let .failure(error):
+                store.deleteCachedFeed { _ in }
                 completion(.failure(error))
                 
             case let .found(feed, timestamp) where validate(timestamp):
