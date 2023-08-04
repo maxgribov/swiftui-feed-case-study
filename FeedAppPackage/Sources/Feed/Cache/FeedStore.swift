1d67886376382171db4d02c085ee67bd8f521107
Max Gribov <gribov.max@gmail.com>
2023-08-04 13:59:50 +0300

    Add comments to document than FeedStore methods callbacks returns result on any thread

--- FeedAppPackage/Sources/Feed/Cache/FeedStore.swift
+++ FeedAppPackage/Sources/Feed/Cache/FeedStore.swift
@@ -20,7 +20,15 @@ public protocol FeedStore {
     typealias InsetrionCompletion = (Error?) -> Void
     typealias RetrievalCompletion = (RetrieveCachedFeedResult) -> Void
     
+    /// The completion handler con be invoked in any thread.
+    /// Cliens are responsable to dispatch to approtpiate threads, if needed.
     func deleteCachedFeed(completion: @escaping DeletionCompletion)
+    
+    /// The completion handler con be invoked in any thread.
+    /// Cliens are responsable to dispatch to approtpiate threads, if needed.
     func insert(_ items: [LocalFeedImage], timestamp: Date, completion: @escaping InsetrionCompletion)
+    
+    /// The completion handler con be invoked in any thread.
+    /// Cliens are responsable to dispatch to approtpiate threads, if needed.
     func retrieve(completion: @escaping RetrievalCompletion)
 }
