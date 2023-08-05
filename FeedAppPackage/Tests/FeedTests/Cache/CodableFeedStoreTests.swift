1ba2f5e87dcedcdbdd79527ccd7ab6591c77c66b
Max Gribov <gribov.max@gmail.com>
2023-08-05 10:44:46 +0300

    Extract `FailableDeleteFeedStoreSpecs` checks into reusable assertions

--- FeedAppPackage/Tests/FeedTests/Cache/CodableFeedStoreTests.swift
+++ FeedAppPackage/Tests/FeedTests/Cache/CodableFeedStoreTests.swift
@@ -141,9 +141,7 @@ final class CodableFeedStoreTests: XCTestCase, FailableFeedStore {
         let noDeletePermissionsURL = cachesDirectory()
         let sut = makeSUT(storeURL: noDeletePermissionsURL)
         
-        let deletionError = deleteCache(from: sut)
-        
-        XCTAssertNotNil(deletionError, "Expected cache deletion to fail")
+        assertThatDeleteDeliversErrorOnDeletionError(on: sut)
     }
     
     func test_delete_hasNoSideEffectsOnDeletionError() {
@@ -151,9 +149,7 @@ final class CodableFeedStoreTests: XCTestCase, FailableFeedStore {
         let noDeletePermissionsURL = cachesDirectory()
         let sut = makeSUT(storeURL: noDeletePermissionsURL)
         
-        deleteCache(from: sut)
-        
-        expect(sut, toRetrieve: .empty)
+        assertThatDeleteHasNoSideEffectsOnDeletionError(on: sut)
     }
     
     func test_storeSideEffects_runSerially() {
---
+++ FeedAppPackage/Tests/FeedTests/Cache/XCTestCase+FailableDeleteFeedStoreSpecs.swift
@@ -0,0 +1,26 @@
+//
+//  XCTestCase+FailableDeleteFeedStoreSpecs.swift
+//  
+//
+//  Created by Max Gribov on 05.08.2023.
+//
+
+import XCTest
+import Feed
+
+extension FailableDeleteFeedStoreSpecs where Self: XCTestCase {
+    
+    func assertThatDeleteDeliversErrorOnDeletionError(on sut: FeedStore, file: StaticString = #file, line: UInt = #line) {
+        
+        let deletionError = deleteCache(from: sut)
+        
+        XCTAssertNotNil(deletionError, "Expected cache deletion to fail", file: file, line: line)
+    }
+    
+    func assertThatDeleteHasNoSideEffectsOnDeletionError(on sut: FeedStore, file: StaticString = #file, line: UInt = #line) {
+        
+        deleteCache(from: sut)
+        
+        expect(sut, toRetrieve: .empty, file: file, line: line)
+    }
+}
