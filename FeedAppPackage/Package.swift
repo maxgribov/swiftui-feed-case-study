86dc282eec3314a186303999f299fc9d663550dd
Max Gribov <gribov.max@gmail.com>
2023-08-07 19:12:17 +0300

    Add 'FeedStore' data model

--- FeedAppPackage/Package.swift
+++ FeedAppPackage/Package.swift
@@ -17,7 +17,8 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "Feed", dependencies: []),
+            name: "Feed",
+            resources: [.process("Feed/FeedStore.xcdatamodeld")]),
         .testTarget(name: "FeedTests", dependencies: ["Feed"]),
         .testTarget(name: "FeedAPIEndToEndTests", dependencies: ["Feed"]),
     ]
