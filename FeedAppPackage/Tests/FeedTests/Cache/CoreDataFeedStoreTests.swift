58fb49be50ef0cf95c27c1f6de8295cabc4a06ac
Max Gribov <gribov.max@gmail.com>
2023-08-08 12:08:35 +0300

    CoreDataFeedCache deledCachedFeed empties cached data

--- FeedAppPackage/Tests/FeedTests/Cache/CoreDataFeedStoreTests.swift
+++ FeedAppPackage/Tests/FeedTests/Cache/CoreDataFeedStoreTests.swift
@@ -82,6 +82,9 @@ final class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
     
     func test_delete_emptiesPreviouslyInsertedCache() {
         
+        let sut = makeSUT()
+        
+        assertThatDeleteEmptiesPreviouslyInsertedCache(on: sut)
     }
     
     func test_storeSideEffects_runSerially() {
