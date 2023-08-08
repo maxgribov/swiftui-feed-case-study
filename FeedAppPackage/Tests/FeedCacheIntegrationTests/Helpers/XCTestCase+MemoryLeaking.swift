b64d4fcfab281e9ae48b207335084af218d275a6
Max Gribov <gribov.max@gmail.com>
2023-08-08 14:53:25 +0300

    Include memory leak tracking helper in the cache integration tests target

---
+++ FeedAppPackage/Tests/FeedCacheIntegrationTests/Helpers/XCTestCase+MemoryLeaking.swift
@@ -0,0 +1,20 @@
+//
+//  XCTestCase+MemoryLeaking.swift
+//  
+//
+//  Created by Max Gribov on 08.08.2023.
+//
+
+import XCTest
+
+extension XCTestCase {
+    
+    func trackForMemoryLeaks( _ instanse: AnyObject, file: StaticString = #filePath, line: UInt = #line) {
+        
+        addTeardownBlock { [weak instanse] in
+             
+            XCTAssertNil(instanse, "Instance should have been deallocated. Potential memory leak", file: file, line: line)
+        }
+    }
+}
+
