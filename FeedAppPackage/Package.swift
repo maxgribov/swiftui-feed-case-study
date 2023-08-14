29400c14d217b2a117d1e8240798ec53c0064f8b
Max Gribov <gribov.max@gmail.com>
2023-08-14 19:20:43 +0300

    Add 'FeedIOS' module for iOS specific components

--- FeedAppPackage/Package.swift
+++ FeedAppPackage/Package.swift
@@ -9,6 +9,7 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(name: "Feed", targets: ["Feed"]),
+        .library(name: "FeedIOS", targets: ["FeedIOS"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -18,8 +19,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(name: "Feed"),
+        .target(name: "FeedIOS"),
         .testTarget(name: "FeedTests", dependencies: ["Feed"]),
         .testTarget(name: "FeedAPIEndToEndTests", dependencies: ["Feed"]),
         .testTarget(name: "FeedCacheIntegrationTests", dependencies: ["Feed"]),
+        .testTarget(name: "FeedIOSTests", dependencies: ["FeedIOS"]),
     ]
 )
---
+++ FeedAppPackage/Sources/FeedIOS/FeedViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  FeedViewModel.swift
+//  
+//
+//  Created by Max Gribov on 14.08.2023.
+//
+
+import Foundation
