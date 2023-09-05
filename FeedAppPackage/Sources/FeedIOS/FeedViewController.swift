a4792d5e841dd652529925db4927e0e112935033
Max Gribov <gribov.max@gmail.com>
2023-09-05 19:08:30 +0300

    Extract `cancelImageDataLoad(from: URL)` method from `FeedImageDataLoader` protocol into a new `FeedImageDataLoaderTask` protocol that represents a task that can be cancelled. This way, we respect the Interface Segregation Principle and `FeedImageDataLoader` implementations are not forced to be stateful.

--- FeedAppPackage/Sources/FeedIOS/FeedViewController.swift
+++ FeedAppPackage/Sources/FeedIOS/FeedViewController.swift
@@ -8,10 +8,14 @@
 import UIKit
 import Feed
 
+public protocol FeedImageDataLoaderTask {
+    
+    func cancel()
+}
+
 public protocol FeedImageDataLoader {
     
-    func loadImageData(from url: URL)
-    func cancelImageDataLoad(from url: URL)
+    func loadImageData(from url: URL) -> FeedImageDataLoaderTask
 }
 
 public final class FeedViewController: UITableViewController {
@@ -19,6 +23,7 @@ public final class FeedViewController: UITableViewController {
     private var feedLoader: FeedLoader?
     private var imageLoader: FeedImageDataLoader?
     private var tableModel = [FeedImage]()
+    private var tasks = [IndexPath: FeedImageDataLoaderTask]()
     
     public convenience init(feedLoader: FeedLoader, imageLoader: FeedImageDataLoader) {
         
@@ -63,14 +68,14 @@ public final class FeedViewController: UITableViewController {
         cell.locationContainer.isHidden = (cellModel.location == nil)
         cell.locationLabel.text = cellModel.location
         cell.descriptionLabel.text = cellModel.description
-        imageLoader?.loadImageData(from: cellModel.url)
+        tasks[indexPath] = imageLoader?.loadImageData(from: cellModel.url)
         
         return cell
     }
     
     public override func tableView(_ tableView: UITableView, didEndDisplaying cell: UITableViewCell, forRowAt indexPath: IndexPath) {
         
-        let cellModel = tableModel[indexPath.row]
-        imageLoader?.cancelImageDataLoad(from: cellModel.url)
+        tasks[indexPath]?.cancel()
+        tasks[indexPath] = nil
     }
 }
--- FeedAppPackage/Tests/FeedIOSTests/FeedViewControllerTests.swift
+++ FeedAppPackage/Tests/FeedIOSTests/FeedViewControllerTests.swift
@@ -181,17 +181,26 @@ final class FeedViewControllerTests: XCTestCase {
         
         //MARK: - FeedImageDataLoader
         
+        private struct TaskSpy: FeedImageDataLoaderTask {
+            
+            let cancelCallback: () -> Void
+            
+            func cancel() {
+                
+                cancelCallback()
+            }
+        }
+        
         private(set) var loadedImageURLs = [URL]()
         private(set) var cancelledImagesURLs = [URL]()
         
-        func loadImageData(from url: URL) {
+        func loadImageData(from url: URL) -> FeedImageDataLoaderTask {
             
             loadedImageURLs.append(url)
-        }
-        
-        func cancelImageDataLoad(from url: URL) {
             
-            cancelledImagesURLs.append(url)
+            return TaskSpy { [weak self] in
+                self?.cancelledImagesURLs.append(url)
+            }
         }
     }
 }
