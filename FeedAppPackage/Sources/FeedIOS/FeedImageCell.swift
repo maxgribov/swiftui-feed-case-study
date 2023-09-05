14695ead20b4f6a32684689736d5b23d74fefc31
Max Gribov <gribov.max@gmail.com>
2023-09-05 16:07:11 +0300

    Render loaded feed on successful load completion

---
+++ FeedAppPackage/Sources/FeedIOS/FeedImageCell.swift
@@ -0,0 +1,15 @@
+//
+//  FeedImageCell.swift
+//  
+//
+//  Created by Max Gribov on 05.09.2023.
+//
+
+import UIKit
+
+public final class FeedImageCell: UITableViewCell {
+    
+    public let locationContainer = UIView()
+    public let locationLabel = UILabel()
+    public let descriptionLabel = UILabel()
+}
