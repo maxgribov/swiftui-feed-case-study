ae911f0c117654d5b4a5ea5083463db3ddc90344
Max Gribov <gribov.max@gmail.com>
2023-09-19 14:40:14 +0300

    `FeedPresenter` displays no error on `sisStartLoadingFeed`

--- FeedAppPackage/Tests/FeedTests/Presentation/FeedPresenterTests.swift
+++ FeedAppPackage/Tests/FeedTests/Presentation/FeedPresenterTests.swift
@@ -7,10 +7,30 @@
 
 import XCTest
 
+protocol FeedErrorView {
+    
+    func display(_ viewModel: FeedErrorViewModel)
+}
+
+struct FeedErrorViewModel {
+    
+    let message: String?
+    
+    static let noError: FeedErrorViewModel = .init(message: nil)
+}
+
 final class FeedPresenter {
     
-    init(view: Any) {
+    private let errorView: FeedErrorView
+    
+    init(errorView: FeedErrorView) {
         
+        self.errorView = errorView
+    }
+    
+    func didStartLoadingFeed() {
+        
+        errorView.display(.noError)
     }
 }
 
@@ -23,20 +43,38 @@ final class FeedPresenterTests: XCTestCase {
         XCTAssertTrue(view.messages.isEmpty, "Expected no view messages")
     }
     
+    func test_didStartLoadingFeed_displayNoErrorMessage() {
+        let (sut, view) = makeSUT()
+        
+        sut.didStartLoadingFeed()
+        
+        XCTAssertEqual(view.messages, [.display(errorMessage: .none)])
+    }
+    
     //MARK: - Helpers
     
     private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (sut: FeedPresenter, view: ViewSpy) {
      
         let view = ViewSpy()
-        let sut = FeedPresenter(view: view)
+        let sut = FeedPresenter(errorView: view)
         trackForMemoryLeaks(sut, file: file, line: line)
         trackForMemoryLeaks(view, file: file, line: line)
         
         return (sut, view)
     }
     
-    private class ViewSpy {
+    private class ViewSpy: FeedErrorView {
         
-        let messages = [Any]()
+        enum Message: Equatable {
+            
+            case display(errorMessage: String?)
+        }
+        
+        private(set) var messages = [Message]()
+        
+        func display(_ viewModel: FeedErrorViewModel) {
+            
+            messages.append(.display(errorMessage: viewModel.message))
+        }
     }
 }
