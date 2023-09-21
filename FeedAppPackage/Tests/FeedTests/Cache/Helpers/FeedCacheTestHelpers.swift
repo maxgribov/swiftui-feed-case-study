e8afc1928114a43e8762fa676dc90878375a89de
Max Gribov <gribov.max@gmail.com>
2023-09-21 14:32:06 +0300

    Move FeedImage creation helpers  to `SharedTestHelpers` file

--- FeedAppPackage/Tests/FeedTests/Cache/Helpers/FeedCacheTestHelpers.swift
+++ FeedAppPackage/Tests/FeedTests/Cache/Helpers/FeedCacheTestHelpers.swift
@@ -6,20 +6,6 @@
 //
 
 import Foundation
-import Feed
-
-func uniqueFeedItem() -> FeedImage {
-    
-    FeedImage(id: UUID(), url: anyURL())
-}
-
-func uniqueFeedItems() -> (models: [FeedImage], local: [LocalFeedImage]) {
-    
-    let items = [uniqueFeedItem(), uniqueFeedItem()]
-    let localItems = items.map { LocalFeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.url) }
-    
-    return (items, localItems)
-}
 
 extension Date {
     
--- FeedAppPackage/Tests/FeedTests/Helpers/SharedTestHelpers.swift
+++ FeedAppPackage/Tests/FeedTests/Helpers/SharedTestHelpers.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import Feed
 
 func anyURL() -> URL {
     
@@ -15,4 +16,17 @@ func anyURL() -> URL {
 func anyNSError() -> NSError {
     
     NSError(domain: "any error", code: 0)
+}
+
+func uniqueFeedItem() -> FeedImage {
+    
+    FeedImage(id: UUID(), url: anyURL())
+}
+
+func uniqueFeedItems() -> (models: [FeedImage], local: [LocalFeedImage]) {
+    
+    let items = [uniqueFeedItem(), uniqueFeedItem()]
+    let localItems = items.map { LocalFeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.url) }
+    
+    return (items, localItems)
 }
