ec3613b6772452e0ef6951c39369497fee451be7
Max Gribov <gribov.max@gmail.com>
2023-09-21 14:40:06 +0300

    `didStartLoadingImage` display FeedImage with loading state

--- FeedAppPackage/Tests/FeedTests/Presentation/FeedImagePresenterTests.swift
+++ FeedAppPackage/Tests/FeedTests/Presentation/FeedImagePresenterTests.swift
@@ -6,15 +6,39 @@
 //
 
 import XCTest
+import Feed
 
 protocol FeedImageView {
     
+    func display(_ viewModel: FeedImageViewModel)
+}
+
+struct FeedImageViewModel {
+    
+    let locationText: String?
+    let descriptionText: String?
+    let imageState: ImageState
+    
+    enum ImageState {
+        case loading
+    }
 }
 
 final class FeedImagePresenter {
     
+    private let view: FeedImageView
+    
     init(view: FeedImageView) {
         
+        self.view = view
+    }
+    
+    func didStartLoadingImage(for model: FeedImage) {
+        
+        view.display(FeedImageViewModel(
+            locationText: model.location,
+            descriptionText: model.description,
+            imageState: .loading))
     }
 }
 
@@ -23,15 +47,36 @@ final class FeedImagePresenterTests: XCTestCase {
     func test_init_doesNotSentMessagesToView() {
         
         let view = ViewSpy()
-        let sut = FeedImagePresenter(view: view)
+        let _ = FeedImagePresenter(view: view)
         
         XCTAssertTrue(view.messages.isEmpty)
+    }
+    
+    func test_didStartLoadingImage_displaysFeedImageLoadingState() {
+        
+        let view = ViewSpy()
+        let sut = FeedImagePresenter(view: view)
+        
+        let feedItem = uniqueFeedItem()
+        sut.didStartLoadingImage(for: feedItem)
+        
+        XCTAssertEqual(view.messages, [.displayLoading(feedItem.location, feedItem.description)])
     }
     
     //MARK: - Helpers
     
     private class ViewSpy: FeedImageView {
         
-        let messages = [Any]()
+        private(set) var messages = [Message]()
+        
+        enum Message: Equatable {
+            
+            case displayLoading(String?, String?)
+        }
+        
+        func display(_ viewModel: FeedImageViewModel) {
+            
+            messages.append(.displayLoading(viewModel.locationText, viewModel.descriptionText))
+        }
     }
 }
