6692f378e30162f69b4ccdd3c78654e5a3900cc9
Max Gribov <gribov.max@gmail.com>
2023-09-28 14:56:47 +0300

    loadImageData returns `connectivity` error on client error

--- FeedAppPackage/Tests/FeedTests/API/RemoteFeedImageDataLoaderTests.swift
+++ FeedAppPackage/Tests/FeedTests/API/RemoteFeedImageDataLoaderTests.swift
@@ -6,13 +6,43 @@
 //
 
 import XCTest
+import Feed
 
 final class RemoteFeedImageDataLoader {
     
-    init(client: RemoteFeedImageDataLoaderTests.HTTPClientSpy) {
+    private let client: HTTPClient
+    
+    init(client: HTTPClient) {
         
+        self.client = client
     }
     
+    func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
+        
+        client.get(from: url) { result in
+            
+            switch result {
+            case .failure:
+                completion(.failure(Error.connectivity))
+                
+            default:
+                break
+            }
+        }
+        
+        return Task()
+    }
+    
+    struct Task: FeedImageDataLoaderTask {
+        
+        func cancel() {
+            
+        }
+    }
+    
+    enum Error: Swift.Error {
+        case connectivity
+    }
 }
 
 final class RemoteFeedImageDataLoaderTests: XCTestCase {
@@ -25,10 +55,50 @@ final class RemoteFeedImageDataLoaderTests: XCTestCase {
         XCTAssertEqual(client.requests.count, 0)
     }
     
+    func test_loadImageData_returnConnectivityErrorOnClientError() {
+        
+        let client = HTTPClientSpy()
+        let sut = RemoteFeedImageDataLoader(client: client)
+        
+        let exp = expectation(description: "Request completion")
+        _ = sut.loadImageData(from: anyURL()) { result in
+            
+            exp.fulfill()
+            
+            switch result {
+            case let .failure(error):
+                guard let imageLoaderError = error as? RemoteFeedImageDataLoader.Error else {
+                    return XCTFail("Expected image loader error, got \(error) instead")
+                }
+                guard imageLoaderError == .connectivity else {
+                    return XCTFail("Expected connectivity error, got \(imageLoaderError) instead")
+                }
+                
+                break
+            
+            default:
+                XCTFail("Expected connectivity error, got \(result) instead")
+            }
+        }
+        
+        client.complete(with: anyNSError())
+        wait(for: [exp], timeout: 1.0)
+    }
+    
     //MARK: - Helpers
     
-    class HTTPClientSpy {
+    class HTTPClientSpy: HTTPClient {
         
-        var requests = [Any]()
+        private(set) var requests = [(url: URL, completion: (HTTPClient.Result) -> Void)]()
+        
+        func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) {
+            
+            requests.append((url, completion))
+        }
+        
+        func complete(with error: Error, at index: Int = 0) {
+            
+            requests[index].completion(.failure(error))
+        }
     }
 }
