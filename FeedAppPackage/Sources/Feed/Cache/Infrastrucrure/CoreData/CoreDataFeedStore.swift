b48ee93487edf3d2e484ffbaa5baf9f4d8075eee
Max Gribov <gribov.max@gmail.com>
2023-10-06 18:52:45 +0300

    Load `NSManagedObjectModel` instance lazily and cache it to prevent multiple `NSEntityDescriptions` claiming the same `NSManagedObject` model subclasses (This problem just generates warnings but could lead to undefined behavior in the future).  dabc2a

--- FeedAppPackage/Sources/Feed/Cache/Infrastrucrure/CoreData/CoreDataFeedStore.swift
+++ FeedAppPackage/Sources/Feed/Cache/Infrastrucrure/CoreData/CoreDataFeedStore.swift
@@ -9,35 +9,54 @@ import CoreData
 
 public final class CoreDataFeedStore {
     
-    private let container: NSPersistentContainer
-    private let context: NSManagedObjectContext
-    
-    public init(storeURL: URL) throws {
+    private static let modelName = "FeedStore"
+    private static let model = NSManagedObjectModel.with(name: modelName, in: bundle())
+    private static func bundle() -> Bundle {
         
         let baseBundle = Bundle(for: CoreDataFeedStore.self)
         let packageBundleURL = baseBundle.resourceURL!.appending(component: "FeedAppPackage_Feed.bundle")
-        let packageBundle = Bundle(url: packageBundleURL)!
-        container = try NSPersistentContainer.load(modelName: "FeedStore", url: storeURL, in: packageBundle)
-        context = container.newBackgroundContext()
+        
+        return Bundle(url: packageBundleURL)!
+    }
+    
+    private let container: NSPersistentContainer
+    private let context: NSManagedObjectContext
+
+    enum StoreError: Error {
+        case modelNotFound
+        case failedToLoadPersistentContainer(Error)
+    }
+
+    public init(storeURL: URL) throws {
+        
+        guard let model = CoreDataFeedStore.model else {
+            throw StoreError.modelNotFound
+        }
+        
+        do {
+            container = try NSPersistentContainer.load(name: CoreDataFeedStore.modelName, model: model, url: storeURL)
+            context = container.newBackgroundContext()
+        } catch {
+            throw StoreError.failedToLoadPersistentContainer(error)
+        }
     }
 
     func perform(_ action: @escaping (NSManagedObjectContext) -> Void) {
         
-        context.perform { [context] in
-            action(context)
-        }
+        let context = self.context
+        context.perform { action(context) }
     }
     
     private func cleanUpReferencesToPersistentStores() {
         
         context.performAndWait {
-            
             let coordinator = self.container.persistentStoreCoordinator
             try? coordinator.persistentStores.forEach(coordinator.remove)
         }
     }
     
     deinit {
+        
         cleanUpReferencesToPersistentStores()
     }
 }
--- FeedAppPackage/Sources/Feed/Cache/Infrastrucrure/CoreData/CoreDataHelpers.swift
+++ FeedAppPackage/Sources/Feed/Cache/Infrastrucrure/CoreData/CoreDataHelpers.swift
@@ -7,26 +7,18 @@
 
 import CoreData
 
+
 extension NSPersistentContainer {
     
-    enum LoadingError: Error {
-        case modelNotFound
-        case failedLoadingPersistenStore(Error)
-    }
-    
-    static func load(modelName: String, url: URL, in bundle: Bundle) throws -> NSPersistentContainer {
-        
-        guard let model = NSManagedObjectModel.with(name: modelName, bundle: bundle) else {
-            throw LoadingError.modelNotFound
-        }
+    static func load(name: String, model: NSManagedObjectModel, url: URL) throws -> NSPersistentContainer {
         
         let description = NSPersistentStoreDescription(url: url)
-        let container = NSPersistentContainer(name: modelName, managedObjectModel: model)
+        let container = NSPersistentContainer(name: name, managedObjectModel: model)
         container.persistentStoreDescriptions = [description]
         
-        var loadStoresError: Error?
-        container.loadPersistentStores { loadStoresError = $1 }
-        try loadStoresError.map{ throw LoadingError.failedLoadingPersistenStore($0) }
+        var loadError: Swift.Error?
+        container.loadPersistentStores { loadError = $1 }
+        try loadError.map { throw $0 }
         
         return container
     }
@@ -34,7 +26,7 @@ extension NSPersistentContainer {
 
 extension NSManagedObjectModel {
     
-    static func with(name: String, bundle: Bundle) -> NSManagedObjectModel? {
+    static func with(name: String, in bundle: Bundle) -> NSManagedObjectModel? {
         
         return bundle
             .url(forResource: name, withExtension: "momd")
