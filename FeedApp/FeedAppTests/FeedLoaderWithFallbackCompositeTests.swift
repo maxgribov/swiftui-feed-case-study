4a3b32f2b4456c71f9af51e3b985e54656278860
Max Gribov <gribov.max@gmail.com>
2023-10-09 13:16:20 +0300

    Extract system under test (sut) creation into a factory method

--- FeedApp/FeedAppTests/FeedLoaderWithFallbackCompositeTests.swift
+++ FeedApp/FeedAppTests/FeedLoaderWithFallbackCompositeTests.swift
@@ -28,10 +28,7 @@ final class FeedLoaderWithFallbackCompositeTests: XCTestCase {
     func test_load_deliverPrimaryFeedOnPrimaryLoaderSuccess() {
         
         let primaryFeed = uniqueFeed()
-        let primaryLoader = FeedLoaderStub(result: .success(primaryFeed))
-        let fallbackLoader = FeedLoaderStub(result: .success(uniqueFeed()))
-        
-        let sut = FeedLoaderWithFallbackComposite(primary: primaryLoader, fallback: fallbackLoader)
+        let sut = makeSUT(primaryResult: .success(primaryFeed), fallbackResult: .success(uniqueFeed()))
         
         let exp = expectation(description: "Wait for completion")
         sut.load { result in
@@ -53,12 +50,29 @@ final class FeedLoaderWithFallbackCompositeTests: XCTestCase {
     
     //MARK: - Helpers
     
+    private func makeSUT(
+        primaryResult: FeedLoader.Result,
+        fallbackResult: FeedLoader.Result,
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) -> FeedLoader {
+        
+        let primaryLoader = LoaderStub(result: primaryResult)
+        let fallbackLoader = LoaderStub(result: fallbackResult)
+        let sut = FeedLoaderWithFallbackComposite(primary: primaryLoader, fallback: fallbackLoader)
+        trackForMemoryLeaks(primaryLoader, file: file, line: line)
+        trackForMemoryLeaks(fallbackLoader, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        
+        return sut
+    }
+    
     private func uniqueFeed() -> [FeedImage] {
         
         [FeedImage(id: UUID(), description: "a description", location: "a location", url: URL(string: "https://a-url.com")!)]
     }
     
-    private class FeedLoaderStub: FeedLoader {
+    private class LoaderStub: FeedLoader {
         
         private let result: FeedLoader.Result
         
