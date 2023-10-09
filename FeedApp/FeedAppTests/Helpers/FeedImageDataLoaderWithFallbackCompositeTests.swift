9c33d1c9c4d585edea4f5f8f4a4c2f908105a11a
Max Gribov <gribov.max@gmail.com>
2023-10-09 14:48:29 +0300

    Extract expectation of load completion result into private helper method

--- FeedApp/FeedAppTests/Helpers/FeedImageDataLoaderWithFallbackCompositeTests.swift
+++ FeedApp/FeedAppTests/Helpers/FeedImageDataLoaderWithFallbackCompositeTests.swift
@@ -32,22 +32,7 @@ final class FeedImageDataLoaderWithFallbackCompositeTests: XCTestCase {
         let fallbackLoader = ImageDataLoaderStub(result: .success(Data("fallback".utf8)))
         
         let sut = FeedImageDataLoaderWithFallbackComposite(primary: primaryLoader, fallback: fallbackLoader)
-        
-        let exp = expectation(description: "Wait for completion")
-        _ = sut.loadImageData(from: anyURL()) { result in
-            
-            switch result {
-            case let .success(receivedData):
-                XCTAssertEqual(receivedData, primaryData)
-                
-            default:
-                XCTFail("Expected \(primaryData), got \(result) instead")
-            }
-            
-            exp.fulfill()
-        }
-        
-        wait(for: [exp], timeout: 1.0)
+        expect(sut, toCompleteWith: .success(primaryData))
     }
 
     //MARK: - Helpers
@@ -70,6 +55,33 @@ final class FeedImageDataLoaderWithFallbackCompositeTests: XCTestCase {
             
             func cancel() {}
         }
+    }
+    
+    private func expect(
+        _ sut: FeedImageDataLoader,
+        toCompleteWith expectedResult: FeedImageDataLoader.Result,
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) {
+        
+        let exp = expectation(description: "Wait for completion")
+        _ = sut.loadImageData(from: anyURL()) { receivedResult in
+            
+            switch (receivedResult, expectedResult) {
+            case let (.success(receivedData), .success(expectedData)):
+                XCTAssertEqual(receivedData, expectedData, file: file, line: line)
+                      
+            case let (.failure(receivedError as NSError), .failure(expectedError as NSError)):
+                XCTAssertEqual(receivedError, expectedError, file: file, line: line)
+                
+            default:
+                XCTFail("Expected \(expectedResult), got \(receivedResult) instead", file: file, line: line)
+            }
+            
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 1.0)
     }
     
     private func anyData() -> Data {
