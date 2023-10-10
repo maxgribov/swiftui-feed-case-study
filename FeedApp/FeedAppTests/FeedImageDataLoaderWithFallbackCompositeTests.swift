1d0e0ac35220762ef12f461154e23430030da883
Max Gribov <gribov.max@gmail.com>
2023-10-10 13:56:24 +0300

    remove redundant stubs and helper methods

--- FeedApp/FeedAppTests/FeedImageDataLoaderWithFallbackCompositeTests.swift
+++ FeedApp/FeedAppTests/FeedImageDataLoaderWithFallbackCompositeTests.swift
@@ -163,24 +163,6 @@ final class FeedImageDataLoaderWithFallbackCompositeTests: XCTestCase {
         return (sut, primaryLoader, fallbackLoader)
     }
     
-    private func _makeSUT(
-        primaryResult: FeedImageDataLoader.Result,
-        fallbackResult: FeedImageDataLoader.Result,
-        file: StaticString = #filePath,
-        line: UInt = #line
-    ) -> FeedImageDataLoader {
-        
-        let primaryLoader = ImageDataLoaderStub(result: primaryResult)
-        let fallbackLoader = ImageDataLoaderStub(result: fallbackResult)
-        let sut = FeedImageDataLoaderWithFallbackComposite(primary: primaryLoader, fallback: fallbackLoader)
-        
-        trackForMemoryLeaks(primaryLoader, file: file, line: line)
-        trackForMemoryLeaks(fallbackLoader, file: file, line: line)
-        trackForMemoryLeaks(sut, file: file, line: line)
-        
-        return sut
-    }
-    
     private class FeedImageLoaderSpy: FeedImageDataLoader {
         
         private(set) var messages = [(url: URL, completion: (FeedImageDataLoader.Result) -> Void)]()
@@ -242,53 +224,6 @@ final class FeedImageDataLoaderWithFallbackCompositeTests: XCTestCase {
         }
         
         action()
-        
-        wait(for: [exp], timeout: 1.0)
-    }
-    
-    private class ImageDataLoaderStub: FeedImageDataLoader {
-        
-        let result: FeedImageDataLoader.Result
-        
-        init(result: FeedImageDataLoader.Result) {
-            self.result = result
-        }
-        
-        func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
-            
-            completion(result)
-            return Task()
-        }
-        
-        private struct Task: FeedImageDataLoaderTask {
-            
-            func cancel() {}
-        }
-    }
-    
-    private func expect(
-        _ sut: FeedImageDataLoader,
-        toCompleteWith expectedResult: FeedImageDataLoader.Result,
-        file: StaticString = #filePath,
-        line: UInt = #line
-    ) {
-        
-        let exp = expectation(description: "Wait for completion")
-        _ = sut.loadImageData(from: anyURL()) { receivedResult in
-            
-            switch (receivedResult, expectedResult) {
-            case let (.success(receivedData), .success(expectedData)):
-                XCTAssertEqual(receivedData, expectedData, file: file, line: line)
-                      
-            case let (.failure(receivedError as NSError), .failure(expectedError as NSError)):
-                XCTAssertEqual(receivedError, expectedError, file: file, line: line)
-                
-            default:
-                XCTFail("Expected \(expectedResult), got \(receivedResult) instead", file: file, line: line)
-            }
-            
-            exp.fulfill()
-        }
         
         wait(for: [exp], timeout: 1.0)
     }
