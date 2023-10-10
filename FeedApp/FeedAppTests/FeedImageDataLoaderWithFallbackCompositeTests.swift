cc61429d511a4340a6bb641c4045a02f2bb3bd7d
Max Gribov <gribov.max@gmail.com>
2023-10-10 13:51:46 +0300

    load delivers primary image data on primary loader success

--- FeedApp/FeedAppTests/FeedImageDataLoaderWithFallbackCompositeTests.swift
+++ FeedApp/FeedAppTests/FeedImageDataLoaderWithFallbackCompositeTests.swift
@@ -110,9 +110,12 @@ final class FeedImageDataLoaderWithFallbackCompositeTests: XCTestCase {
     func test_load_deliversPrimaryImageDataOnPrimaryLoaderSuccess() {
         
         let primaryData = Data("primary".utf8)
-        let sut = _makeSUT(primaryResult: .success(primaryData), fallbackResult: .success(anyData()))
+        let (sut, primaryLoader, _) = makeSUT()
         
-        expect(sut, toCompleteWith: .success(primaryData))
+        expect(sut, toCompleteWith: .success(primaryData), on: {
+            
+            primaryLoader.complete(with: primaryData)
+        })
     }
     
     func test_load_deliversFallbackImageDataOnPrimaryLoaderFailure() {
@@ -199,6 +202,41 @@ final class FeedImageDataLoaderWithFallbackCompositeTests: XCTestCase {
             
             messages[index].completion(.failure(error))
         }
+        
+        func complete(with data: Data, at index: Int = 0) {
+            
+            messages[index].completion(.success(data))
+        }
+    }
+    
+    private func expect(
+        _ sut: FeedImageDataLoader,
+        toCompleteWith expectedResult: FeedImageDataLoader.Result,
+        on action: () -> Void,
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) {
+        
+        let exp = expectation(description: "Wait for completion")
+        _ = sut.loadImageData(from: anyURL()) { receivedResult in
+            
+            switch (receivedResult, expectedResult) {
+            case let (.success(receivedData), .success(expectedData)):
+                XCTAssertEqual(receivedData, expectedData, file: file, line: line)
+                      
+            case let (.failure(receivedError as NSError), .failure(expectedError as NSError)):
+                XCTAssertEqual(receivedError, expectedError, file: file, line: line)
+                
+            default:
+                XCTFail("Expected \(expectedResult), got \(receivedResult) instead", file: file, line: line)
+            }
+            
+            exp.fulfill()
+        }
+        
+        action()
+        
+        wait(for: [exp], timeout: 1.0)
     }
     
     private class ImageDataLoaderStub: FeedImageDataLoader {
